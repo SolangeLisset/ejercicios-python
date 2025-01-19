b092fe1ae738263ef6ceed894e261d326a38945d
SolangeLi <solangelisset@gmail.com>
2025-01-19 20:16:47 -0300

    Primeros ejericicos de python

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Ingresa un numero"))
+num2 = int(input("Ingresa otro numero"))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print(f"La suma de ambos numeros es de {suma}")
+print(f"La resta de ambos numeros es de {resta}")
+print(f"La multiplicación de ambos numeros es de {multiplicacion}")
+print(f"La división de ambos numeros es de {division}")
