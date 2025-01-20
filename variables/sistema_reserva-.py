609e9273301baa10926828bc934977abc9910137
SolangeLisset <solangeelissett@gmail.com>
2025-01-20 20:02:56 -0300

    Se subio otros 2 ejercicios

---
+++ variables/sistema_reserva-.py
@@ -0,0 +1,11 @@
+print("*** Sistema de Reserva de Hoteles ***")
+#definimos las variables
+nombre_cliente = "Juan martinez"
+dias_de_estancia = 20
+tarifa_diaria = 400,2
+vista_al_mar = True
+
+print("Cliente:",nombre_cliente)
+print("Dias de estancia:", dias_de_estancia)
+print("Tarifa diaria:",tarifa_diaria)
+print("Habitacion con vista al mar?",vista_al_mar)
---
+++ variables/sistema_tienda_online.py
@@ -0,0 +1,13 @@
+#declarar las variables del producto
+
+producto = "Cámara Digital"
+precio_producto = 399.99
+cantidad_inventario = 20
+disponible_entrega = True
+
+print("*** Sistema de tienda online ***")
+
+print("Producto:", producto)
+print("Precio:", "$",precio_producto)
+print("Cantidad inventario:", cantidad_inventario)
+print("Disponible:", disponible_entrega)
