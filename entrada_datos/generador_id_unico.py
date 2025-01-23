ffa01e850a4c26a38e82a0f13aaaeee092925acd
SolangeLisset <solangeelissett@gmail.com>
2025-01-22 21:19:47 -0300

    Se modifico el archivo de generador de id unico

--- entrada_datos/generador_id_unico.py
+++ entrada_datos/generador_id_unico.py
@@ -1,5 +1,15 @@
+from random import randint
+
 print("*** Sistema de generador de ID unico ***")
 
 nombre = input("Ingrese su nombre: ")
 apellido = input("Ingrese su apellido")
-anio_nacimiento = int(input("Ingrese su año de nacimiento yyyy"))
+anio_nacimiento = (input("Ingrese su año de nacimiento yyyy"))
+
+nombre = nombre.strip().upper()[0:2]
+apellido = apellido.strip().upper()[0:2]
+anio_nacimiento = anio_nacimiento.strip()[2:]
+
+aleatorio = randint(1000,9999)
+id_unico = f"{nombre}{apellido}{anio_nacimiento}{aleatorio}"
+print(id_unico)
